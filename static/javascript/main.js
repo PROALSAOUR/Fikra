0341be33c0f4283c068795091d3929335c44f3b9
alsaour <proalsaour@gmail.com>
2025-03-15 00:59:16 +0200

    improve js files

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -81,7 +81,7 @@ function getCookie(name) {
         }
     }
     return cookieValue;
-  }
+}
 // =========================================================================================================
 // الدالة المسؤلة عن اعادة المستخدم الى الصفحة التي جاء منها 
 function goBack() {
@@ -295,6 +295,62 @@ function toggleContent(element) {
         icon.classList.remove('fa-chevron-down');
         icon.classList.add('fa-chevron-up');
     }
+}
+// =========================================================================================================
+//  انشاء تأثير قلب البطاقة لصفحة تسجيل الدخول 
+document.addEventListener('DOMContentLoaded', function() {
+    const flipButtons = document.querySelectorAll('.flip');
+    const signCard = document.querySelector('.sign-card');
+    
+    // استرجاع حالة البطاقة من localStorage
+    const isFlipped = localStorage.getItem('isFlipped') === 'true';
+    
+    // تطبيق حالة البطاقة عند تحميل الصفحة
+    if (isFlipped && signCard) {
+      signCard.classList.add('rotate');
+    }
+    
+    if (flipButtons.length > 0 && signCard) {
+      flipButtons.forEach(button => {
+        button.addEventListener('click', function() {
+          signCard.classList.toggle('rotate');
+          
+          // حفظ الحالة الجديدة في localStorage
+          const isCurrentlyFlipped = signCard.classList.contains('rotate');
+          localStorage.setItem('isFlipped', isCurrentlyFlipped.toString());
+          console.log(isCurrentlyFlipped);
+        });
+      });
+    }
+});
+
+// =========================================================================================================
+//  كود  تغيير حالة الرسالة الى مقروئة عند النقر عليها
+function markMessageAsRead(markUrl, element) {
+      const notificationMessage = element.parentElement;
+    
+      // تحقق مما إذا كانت الرسالة قد قرئت مسبقًا
+      if (!notificationMessage.classList.contains('read')) {
+          // أضف كلاس 'read' لتغيير حالة الرسالة
+          notificationMessage.classList.add('read');
+    
+          // إرسال طلب AJAX إلى الخادم لتحديث حالة الرسالة
+          fetch(markUrl, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'X-CSRFToken': getCookie('csrftoken') // تضمين CSRF token
+              },
+          })
+          .then(response => {
+              if (!response.ok) {
+                  console.error('Failed to mark as read.');
+              }
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          });
+      }
 }
 // =========================================================================================================
   
